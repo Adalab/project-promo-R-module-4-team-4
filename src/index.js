2df0d2756cd9b4ec7c64751d8a5e3670aa643245
Blanca Ubis <ubis.blanca@gmail.com>
2022-12-23 13:48:21 +0100

    Add database and dinamic server for user cards

--- src/index.js
+++ src/index.js
@@ -1,15 +1,16 @@
-
 const express = require('express');
 const cors = require('cors');
 const { v4: uuidv4 } = require('uuid');
+const Database = require('better-sqlite3');
 const savedCards = [];
-
 
 const server = express();
 
 server.use(cors());
-server.use(express.json({limit: '25mb'}));
+server.use(express.json({ limit: '25mb' }));
+server.set('view engine', 'ejs');
 
+const db = new Database('./src/db/cards.db', { verbose: console.log });
 
 const serverPort = 4000;
 server.listen(serverPort, () => {
@@ -18,47 +19,52 @@ server.listen(serverPort, () => {
 
 // Escribimos los endpoints que queramos
 server.post('/card', (req, res) => {
-    //req.body esto es data en el servidor
-let success = true;
-const errorArray = [];
-for (const property in req.body) { 
-  if (req.body[property] === "" ){
-    success = false
-   errorArray.push(property)}
+  //req.body esto es data en el servidor
+  let success = true;
+  const errorArray = [];
+  for (const property in req.body) {
+    if (req.body[property] === '') {
+      success = false;
+      errorArray.push(property);
+    }
     console.log(req.body[property]);
-};
+  }
 
-  if( success === true){
+  if (success === true) {
     const newCard = {
       id: uuidv4(),
-      ...req.body
-    }
+      ...req.body,
+    };
     savedCards.push(newCard);
     const responseSuccess = {
       success: true,
-      cardURL: `http://localhost:4000/card/${newCard.id}`
+      cardURL: `http://localhost:4000/card/${newCard.id}`,
     };
-    res.json(responseSuccess);  
-  }
-  
-  else {
+    res.json(responseSuccess);
+  } else {
     const responseFail = {
-      success : false,
-      error: `Error falta información ${errorArray.join()}`
-  };
-  res.json(responseFail);  
-  }     
-  });  
-
-
-
-server.get('/card', (req, res) => {
-  const response = {
-    error: "aun no tengo informacion"
-     };
-  res.json(response);
+      success: false,
+      error: `Error falta información ${errorArray.join()}`,
+    };
+    res.json(responseFail);
+  }
 });
-console.log("holis");
+
+server.get('/card/:id', (req, res) => {
+  //guardamos el parámetro id de la url
+  const id = req.params.id;
+  //creamos la query
+  const query = db.prepare('SELECT * FROM userCards WHERE id = ?');
+  //ejecuto la query y me devuelve los datos de la tarjeta que correspondan con el id de la url
+  const userCard = query.get(id);
+  console.log(userCard);
+  //pinto el template de tarjetas con mis datos personalizados (del id de la url)
+  res.render('cardTemplate', userCard);
+});
+console.log('holis');
 
 const staticServer = './src/public-react';
 server.use(express.static(staticServer));
+
+const staticServerCSS = './src/public-css';
+server.use(express.static(staticServerCSS));
